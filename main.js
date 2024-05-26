a03ee61129109a627cfe0dc820a2300ba825e3da
wangyuzhuo1 <113219030+wangyuzhuo1@users.noreply.github.com>
2024-05-26 19:04:04 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+song="";
+
+function setup() {
+    createCanvas(600, 500);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500)
+}
+
+function preload() {
+    song = loadSound("music2.mp3");
+}
+
+function play() {
+    song.play();
+}
